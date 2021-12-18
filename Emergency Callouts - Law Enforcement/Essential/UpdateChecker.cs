1006192af5d5c77cad3bddded22e4a9cb5164725
FayaDev <MSAZielhuis@gmail.com>
2021-12-18 20:26:28 +0100

    UpdateChecker name fix.

--- Emergency Callouts - Law Enforcement/Essential/UpdateChecker.cs
+++ Emergency Callouts - Law Enforcement/Essential/UpdateChecker.cs
@@ -22,25 +22,28 @@ namespace EmergencyCalloutsLE
             }
             catch (WebException)
             {
-                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "Emergency Callouts - Police", "~y~Error", "Failed to check for updates; Possible network error.");
+                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "Emergency Callouts - Law Enforcement", "~y~Error", "Failed to check for updates; Possible network error.");
                 Game.LogTrivial("[Emergency Callouts - Law Enforcement]: Checked for updates; Failed to check");
             }
 
             if (OnlineVersion != Project.LocalVersion)
             {
-                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "Emergency Callouts - Police", $"~r~{Project.LocalVersion} ~b~by Faya", $"Found update ~g~{OnlineVersion} ~s~available for you!");
+                GameFiber.Wait(5000);
+                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "Emergency Callouts - Law Enforcement", $"~r~{Project.LocalVersion} ~b~by Faya", $"Found update ~g~{OnlineVersion} ~s~available for you!");
                 Game.LogTrivial("[Emergency Callouts - Law Enforcement]: Checked for updates; Found an update");
                 return true;
             }
             else if (OnlineVersion.ToLower() == "file hidden")
             {
-                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "Emergency Callouts - Police", $"~r~{Project.LocalVersion} ~b~by Faya", "New update is being reviewed by LSPDFR!");
+                GameFiber.Wait(5000);
+                Game.DisplayNotification("commonmenu", "mp_alerttriangle", "Emergency Callouts - Law Enforcement", $"~r~{Project.LocalVersion} ~b~by Faya", "New update is being reviewed by LSPDFR!");
                 Game.LogTrivial("[Emergency Callouts - Law Enforcement]: Checked for updates; File is hidden");
                 return true;
             }
             else
             {
-                Game.DisplayNotification("commonmenu", "shop_tick_icon", "Emergency Callouts - Police", $"~g~{Project.LocalVersion} ~b~by Faya", "~y~Reporting for duty!");
+                GameFiber.Wait(5000);
+                Game.DisplayNotification("commonmenu", "shop_tick_icon", "Emergency Callouts - Law Enforcement", $"~g~{Project.LocalVersion} ~b~by Faya", "~y~Reporting for duty!");
                 Game.LogTrivial("[Emergency Callouts - Law Enforcement]: Checked for updates; None available");
                 return false;
             }
