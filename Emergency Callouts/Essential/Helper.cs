f8b0644d9e52a0f9fac222386ff47b67f3a463bb
FayaDev <MSAZielhuis@gmail.com>
2021-12-26 21:53:31 +0100

    Removed Handle.CalloutEnding

--- Emergency Callouts/Essential/Helper.cs
+++ Emergency Callouts/Essential/Helper.cs
@@ -313,19 +313,6 @@ namespace EmergencyCallouts.Essential
 
         internal static class Handle
         {
-            #region CalloutEnding
-            //internal static void CalloutEnding()
-            //{
-            //    MainPlayer.Tasks.PlayAnimation(new AnimationDictionary("random@arrests"), "generic_radio_enter", 0, 5f, 5f, 0f, AnimationFlags.SecondaryTask | AnimationFlags.UpperBodyOnly);
-            //    Game.DisplayNotification($"~b~You~s~: Dispatch, call is code 4.");
-            //    GameFiber.Sleep(2000);
-            //    Play.CodeFourAudio();
-            //    GameFiber.Sleep(2700);
-            //    Functions.StopCurrentCallout();
-            //    GameFiber.Sleep(500);
-            //}
-            #endregion
-
             #region ManualEnding
             internal static void ManualEnding()
             {
