8f39ed66364006b4556809858f3a5de1a958287d
FayaDev <MSAZielhuis@gmail.com>
2022-01-08 23:49:28 +0100

    Compilation date change

--- Emergency Callouts/Essential/Helper.cs
+++ Emergency Callouts/Essential/Helper.cs
@@ -18,7 +18,7 @@ namespace EmergencyCallouts.Essential
 
         internal static string LocalVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString().Substring(0, 5);
 
-        internal static DateTime DateCreated => File.GetCreationTimeUtc("Emergency Callouts.dll");
+        internal static DateTime DateCreated => File.GetCreationTime("Emergency Callouts.dll");
 
         internal static string SettingsPath => "Plugins/LSPDFR/Emergency Callouts/Settings.ini";
 
