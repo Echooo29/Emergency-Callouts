97a1343cab0e2e2498a22e9a68c71e3f33d38920
FayaDev <MSAZielhuis@gmail.com>
2022-01-17 12:32:31 +0100

    Decrease search area maximum distance around suspect

--- Emergency Callouts/Callouts/SuspiciousActivity.cs
+++ Emergency Callouts/Callouts/SuspiciousActivity.cs
@@ -570,7 +570,7 @@ namespace EmergencyCallouts.Callouts
                     if (EntranceBlip.Exists()) { EntranceBlip.Delete(); }
 
                     // Create SearchArea
-                    SearchArea = new Blip(Suspect.Position.Around2D(45f), Settings.SearchAreaSize);
+                    SearchArea = new Blip(Suspect.Position.Around2D(30f), Settings.SearchAreaSize);
                     SearchArea.SetColorYellow();
                     SearchArea.Alpha = 0.5f;
 
