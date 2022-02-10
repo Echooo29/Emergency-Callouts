73eafd736eb5251ea9d12a59fde7e00c2253406c
FayaDev <MSAZielhuis@gmail.com>
2022-02-10 15:40:58 +0100

    Enable developer mode

--- Emergency Callouts/Callouts/Trespassing.cs
+++ Emergency Callouts/Callouts/Trespassing.cs
@@ -449,10 +449,10 @@ namespace EmergencyCallouts.Callouts
                 switch (CalloutScenario)
                 {
                     case 1:
-                        Scenario3();
+                        Scenario1();
                         break;
                     case 2:
-                        Scenario3();
+                        Scenario2();
                         break;
                     case 3:
                         Scenario3();
